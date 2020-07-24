961aa7ba95ea82cdcc8b619962bcd670a96d98e4
Mike Archer <mlarcher531@gmail.com>
2020-07-24 15:04:50 +0000

    Automatically backed up by Learn

--- scratch.rb
+++ scratch.rb
@@ -1,1 +1,10 @@
-puts "Hi!"
+greeting = "unfriendly_greeting"
+case greeting
+when "unfriendly_greeting"
+  puts "What do you want?!"
+when "friendly_greeting"
+  puts "Hi! How are you?"
+else
+  puts "Do I know you?"
+end
+
