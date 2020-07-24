4ece456f263db3a09ed1fbd10df5093ae9bc5500
Mike Archer <mlarcher531@gmail.com>
2020-07-24 15:01:44 +0000

    Automatically backed up by Learn

--- scratch.rb
+++ scratch.rb
@@ -1,8 +1,1 @@
-chance_of_rain = 0.7
-puts "Let's go outside!"
-if chance_of_rain > 0.5
-  puts "Pack an umbrella!"
-else
-  puts "enjoy the fine day!"
-end
-puts "oh, and always wear sunscreen!"
+puts "Hi!"
